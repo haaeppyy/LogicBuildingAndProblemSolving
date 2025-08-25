2e04d18b180afee935cfbc9baf98b9bf6f105473
AYUSH SINGH BHANDARI <156802277+haaeppyy@users.noreply.github.com>
2025-08-25 22:18:29 +0530

    Time: 4 ms (1.84%) | Memory: 10.7 MB (5.39%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    unordered_map<char , string> createMapping(string digits){
+        unordered_map<char, string> mapping;
+        mapping['2'] = "abc";
+        mapping['3'] = "def";
+        mapping['4'] = "ghi";
+        mapping['5'] = "jkl";
+        mapping['6'] = "mno";
+        mapping['7'] = "pqrs";   // 4 chars
+        mapping['8'] = "tuv";
+        mapping['9'] = "wxyz";   // 4 chars
+        return mapping;
+        
+    }
+void createAns(string digits , vector<string> &ans ,string output ,int i){
+        unordered_map<char , string > mapping = createMapping(digits);
+
+        if(i >= digits.length() ){
+            ans.push_back(output);
+            return;
+        }
+        
+            string tempString = mapping[digits[i]];
+          for(auto c : tempString){
+              //iterate over each character , and include and exclude
+              output.push_back(c);
+              createAns(digits , ans , output , i+1);
+              output.pop_back();
+          }
+    
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(!digits.empty()) {
+            string output = "";
+            createAns( digits, ans , output , 0);
+       }
+
+        
+        return ans;
+    }
+};
