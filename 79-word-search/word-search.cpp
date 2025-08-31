5a486114008f929f386128bc3e26a79156aafbf2
AYUSH SINGH BHANDARI <156802277+haaeppyy@users.noreply.github.com>
2025-09-01 00:14:32 +0530

    Time: 856 ms (16.70%) | Memory: 11.6 MB (44.72%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>> &board , vector<vector<bool>>&visit ,int i , int j){
-        if(i< 0 || i >= board.size()) return false;
-        if(j< 0 || j >= board[0].size()) return false;
-        if(visit[i][j] ==  true) return false;
+    bool dfs(vector<vector<char>> &board , int i , int j ,int st ,string word){
+         if (st == word.size()) return true;
+        if(i < 0 || i >= board.size()) return false;
+        if(j < 0 || j >=board[0].size()) return false;
+        if(board[i][j] != word[st]) return false;
 
-        return true;
-    }
-    bool solveBackTrack(vector<vector<char>> &board , vector<vector<bool>>&visit ,int i , int j , string word , int st){
-        if(st >= word.size()) return true;
+        //change in board to avoid visit vector
+        //save and mark the character inside board
+        char tmpStoreChar = board[i][j];
+        board[i][j] = '*';
 
-        int delrow[] = {-1,1,0,0};
-        int delcol[] = {0,0,-1,1};
-        for(int k = 0 ; k<4 ;k++){
-            int newRow = delrow[k] + i;
-            int newCol = delcol[k] + j;
+        bool recAns = dfs(board ,i-1 , j , st+1, word) || dfs(board ,i+1 , j , st+1 ,word) || dfs(board ,i , j -1 , st+1 , word) || dfs(board ,i , j+1 , st+1 , word);
+        //unMark
+        board[i][j] = tmpStoreChar;
+        return recAns;
 
-            if((isSafe(board,visit,newRow,newCol)) &&board[newRow][newCol] == word[st]){
-                visit[newRow][newCol] = true;
-                bool ans = solveBackTrack(board,visit ,newRow,newCol,word ,st+1);
-                if(ans) return true;
-                visit[newRow][newCol] = false;
-
-            }
-        }
-        
-        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>> visit(board.size(),vector<bool>(board[0].size(),false));
-        bool ans =false;
+        int st = 0;
         for(int i = 0 ; i < board.size() ; i++){
-            for(int j = 0; j < board[0].size() ; j++){
-                if(board[i][j] == word[0]){
-                    visit[i][j] = true;
-                    bool res =solveBackTrack(board,visit,i,j,word,1);
-                    if(!res) visit[i][j] = false;
-                    else{
-                        ans = true;
-                        break;
-                    } 
+            for(int j = 0 ; j< board[0].size(); j++){
+                if((board[i][j] == word[st]) && dfs(board, i , j , st,word)){
+                    return true;
                 }
             }
         }
-
-
-        return ans;
+        return false;
     }
-}; 
+};
