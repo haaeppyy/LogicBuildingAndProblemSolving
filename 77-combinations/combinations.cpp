7a7bc7b33d4f4baa9bc5a5ec3ae72a5c3cc8799d
AYUSH SINGH BHANDARI <156802277+haaeppyy@users.noreply.github.com>
2025-08-31 17:43:34 +0530

    Time: 458 ms (5.01%) | Memory: 222.3 MB (5.00%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solveBackTrack(int n, int k,int i ,vector<vector<int>>&res , vector<int>temp){
+        if(temp.size() == k)
+        {
+            res.push_back(temp);
+        }
+        for(int j = i+1 ; j <= n ; j++){
+            temp.push_back(j);
+            solveBackTrack(n , k , j  ,res, temp );
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>res;
+        vector<int>temp;
+        solveBackTrack(n , k , 0 ,res, temp );
+        return res;
+
+    }
+};
