b541ad3182a31513509e5a7087c2a9270da44aee
AYUSH SINGH BHANDARI <156802277+haaeppyy@users.noreply.github.com>
2025-09-04 01:03:56 +0530

    Time: 1 ms (66.41%) | Memory: 14 MB (72.96%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void solve(vector<int>&candidates,vector<vector<int>>&result,vector<int>temp,int index,int target){
-        if( target<0) return;
-        if(target ==0){
-         result.push_back(temp);
+    void solveBackTrack(vector<int>& candidates , int &target , int sum , int i , vector<vector<int>> &result, vector<int>&temp){
+        if(sum > target) return;
+        if(sum == target){
+            result.push_back(temp);
             return;
         }
 
-        for(int i=index; i<candidates.size();i++){
-            temp.push_back(candidates[i]);
-            solve(candidates,result,temp,i,target-candidates[i]);
-            temp.pop_back();
+        for(int j = i ; j < candidates.size() ; j++){
+            if(target >= candidates[j]){
+                temp.push_back(candidates[j]);
+                solveBackTrack(candidates , target , sum + candidates[j] , j , result , temp);
+                temp.pop_back();
+            }
         }
-
+        
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
+        vector<vector<int>> result;
+        int sum = 0;
+        int i = 0;
         vector<int>temp;
-         solve(candidates,result,temp,0,target);
-         return result;
-         
+        solveBackTrack(candidates , target , sum , i , result,temp);
+        return result;
     }
 };
